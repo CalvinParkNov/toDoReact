7fd962eb536fb809c8f2e6cf0e2eb87ee6e6e0c6
Won Kyun Park <wonkyunpark@Wons-MacBook-Pro.local>
2022-06-28 15:58:48 +0900

    accomplished

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,45 @@
-import React from "react";
+import React, {useState} from "react";
 
 function App() {
-  return (
+
+    const [inputText, setInputText] = useState("");
+    const [items, setItems] = useState([]);
+
+    function handelChange(event) {
+        const newValue = event.target.value;
+        setInputText(newValue)
+    }
+    function addItem() {
+      if(inputText){
+        setItems((prevValue) => {
+            return [
+                ...prevValue,
+                inputText
+            ];
+        });
+      }else {
+        alert("Please input to do.");
+        return false;
+      }
+    }
+return (
     <div className="container">
-      <div className="heading">
-        <h1>To-Do List</h1>
-      </div>
-      <div className="form">
-        <input type="text" />
-        <button>
-          <span>Add</span>
-        </button>
-      </div>
-      <div>
-        <ul>
-          <li>A Item</li>
-        </ul>
-      </div>
+        <div className="heading">
+            <h1>To-Do List</h1>
+        </div>
+        <div className="form">
+            <input type="text" onChange={handelChange} value={inputText}/>
+            <button onClick={addItem}>
+                <span>Add</span>
+            </button>
+        </div>
+        <div>
+            <ul>
+                {items.map((item) => <li>{item}</li>)}
+            </ul>
+        </div>
     </div>
-  );
+);
 }
 
 export default App;
